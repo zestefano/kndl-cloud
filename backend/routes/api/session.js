e38cd22a510e6dfee265ae4b85e3889ba7f8cbf6
zachery estefano <zacheryestefano@gmail.com>
2021-12-04 15:32:10 -0500

    login validation working

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -63,13 +78,10 @@ router.get(
 //     method: 'POST',
 //     headers: {
 //       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `rwFnnFna-UUe5bKvZ25lfXuzRbm43-Nq2QLg`
+//       "XSRF-TOKEN": `sehrC1PE-C9PP7NAOW7Zs8XIrFv-nRX67G5A`
 //     },
-//     body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
+//     body: JSON.stringify({ credential: '', password: 'password' })
 //   }).then(res => res.json()).then(data => console.log(data));
-
-
-
 
 
 
