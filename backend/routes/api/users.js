62041e5fc9d40feaed606dc50fc929c4aedade1f
zachery estefano <zacheryestefano@gmail.com>
2021-12-04 15:38:10 -0500

    signup validation working

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
 
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+
 
 // Sign up
 router.post(
     '/',
+    validateSignup,
     asyncHandler(async (req, res) => {
       const { email, password, username } = req.body;
       const user = await User.signup({ email, username, password });
@@ -18,16 +42,27 @@ router.post(
       await setTokenCookie(res, user);
   
       return res.json({
-        user
+        user,
       });
-    })
+    }),
   );
 
 
+  
 
 
-
-
+//   fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `v0phJRIM-kBDjFW5OCjJKV8wZ6_0Hwwue08s`
+//     },
+//     body: JSON.stringify({
+//       email: 'dwd',
+//       username: 'Firestar@fr.com',
+//       password: 'dwdwd'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
 
 
 module.exports = router;
